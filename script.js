3bd459be0eca4d9c99edf5dff92a801d890367fd
Glitch (hello-webpage) <none>
2020-07-30 15:20:13 +0000

    🐕⚽️ Checkpoint ./take-action.html:19026581/6 ./script.js:19026581/106

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 /************************************************/
 
 /* TODO: Add a variable to store the "my-list" element */
-
+var fullItemList=0;
+my-list = document.getElementbyId("my-list")
 
 /* TODO: Create the event listener that listens for a mouse click and runs the checkOffList function */
 
